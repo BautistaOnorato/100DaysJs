e691ecc6deab12920f49a536989b47e931571c00
BautistaOnorato <bautistaonorato27@gmail.com>
2023-07-07 15:44:41 -0300

    correción dia 5

--- day-5_battery-detector/src/components/BatteryPill.jsx
+++ day-5_battery-detector/src/components/BatteryPill.jsx
@@ -3,12 +3,12 @@ import { defineStyle } from "../logic/defineStyle"
 /* eslint-disable react/prop-types */
 const BatteryPill = ({ status }) => {
   return (
-    <div className="battery-pill" onClick={() => console.log(status)}>
+    <div className="battery-pill">
       <div className="battery-liquid" style={defineStyle(status.percentage)}>
         <div className="battery-liquid-top" 
           style={status.percentage === 100 ? 
             { paddingBottom: "0", background: "none" } 
-            : { paddingBottom: "4px", background: defineStyle().background }}
+            : { paddingBottom: "4px", background: defineStyle(status.percentage).background }}
           />
       </div>
     </div>
